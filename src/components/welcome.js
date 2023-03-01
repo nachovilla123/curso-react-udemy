640b631759145aee520a6b7f48b580ffbd1920a0
nachovilla123 <nacho.villarruel@gmail.com>
2023-03-01 19:13:52 -0300

    use State

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+
+
 export default function welcome(props){
-    console.log(props);
+   
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [counter, setCounter] = useState(0);
     const { message , name} = props
     return (
         <div>
             <h1> "Hola {name}" </h1>
-            <p> {message} </p>
-
+            <h1> "contador {counter}" </h1>
+   
+            <h2>Contador de React con Hooks</h2>
+            <button type="submit" onClick={()=> setCounter(counter + 1)}>Aumentar</button>
         </div>
        
     );
