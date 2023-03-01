a087f65f2e2b8e492d3c032e01af9e491e360127
nachovilla123 <nacho.villarruel@gmail.com>
2023-03-01 19:27:07 -0300

    use effect

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,18 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 
 
 export default function welcome(props){
    
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [counter, setCounter] = useState(0);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [semaforo, setSemaforo] = useState(false);
     const { message , name} = props
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        console.log(semaforo);
+    }, [semaforo]);
+
+
+
+    const contar = () => {
+        console.log("entrando en la funcion de contar")
+        setCounter(counter + 1);
+        setSemaforo(!semaforo);// va a cambiar el semaforo a lo contrario! :P 
+    }
     return (
         <div>
             <h1> "Hola {name}" </h1>
-            <h1> "contador {counter}" </h1>
+            <h1> "contador {counter + 1}" </h1>
    
+            <p>El semaforo esta en color {semaforo ? 'red' : 'green'} </p> 
+
+
             <h2>Contador de React con Hooks</h2>
-            <button type="submit" onClick={()=> setCounter(counter + 1)}>Aumentar</button>
+            <button type="submit" onClick={contar}>Aumentar</button>
         </div>
        
     );
