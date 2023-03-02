1b9dbf537c7cd8d668199a0c516db640c4a17029
nachovilla123 <nacho.villarruel@gmail.com>
2023-03-02 16:28:45 -0300

    React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/welcome';
+import { BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom'; 
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -9,6 +11,23 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         
         <Welcome message="hola welcome props " name="nacho" />
+
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contacto</Link>
+              </li>
+            </ul>
+          </div>
+          <Routes>
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+        
       </header>
     </div>
   );
