b9ed58dd9bf2f2abe9ac53f1e60220e210e1b130
nachovilla123 <nacho.villarruel@gmail.com>
2023-03-02 21:22:36 -0300

    pidiendo datos api

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import { BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom';
 import Contact from './pages/Contact';
 import AboutMe from './pages/AboutMe';
 import Profile from './pages/Profie';
+import imageRickMorty from './images/rick-morty.png'
 
 function App() {
+
+  const reqApi = async () => {
+   const api = await fetch('https://rickandmortyapi.com/api/character');
+   const characterApi = await api.json();
+   console.log(characterApi);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <Welcome message="hola welcome props " name="nacho" />
+
+      <h1 className='title'>Rick & Morty</h1>
+      <img src={imageRickMorty} alt='Rick & Morty' className='img-home'></img>
+      <button onClick={reqApi} className="btn-search">Buscar personajes</button>
 
         <Router>
           <div>
