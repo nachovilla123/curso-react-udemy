f04650e2fd34acb4d7e346ac14955d2bbf1dbe77
nachovilla123 <nacho.villarruel@gmail.com>
2023-03-03 12:24:57 -0300

    recorriendo datos de la api

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,10 +1,18 @@
 export default function Characters(props){
-    console.log(props);
-    const {characters} = props
+    
+    const {characters} = props // characters era lo que le pasaba desde el otro componente
 
     return (
-        <div>
-            <h1> Desde characters </h1>
+        <div className="characters">
+            <h1> personajes </h1>
+            <span className="back-home">Home</span>
+            <div className="container-characters">
+                {characters.map((characters,index) =>( // return implicito, es para no hacer un return dentro de un return.
+                    <div className="character-container" key={index}>
+                        <p>{characters.name}</p>
+                    </div>
+                ))}
+            </div>
           
 
 
