a519ad76cdb42feb3c394b928a3402170c55a960
nachovilla123 <nacho.villarruel@gmail.com>
2023-03-03 12:40:09 -0300

    modelando los characters

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -4,20 +4,43 @@ export default function Characters(props){
 
     return (
         <div className="characters">
-            <h1> personajes </h1>
+            <h1> Personajes: </h1>
             <span className="back-home">Home</span>
             <div className="container-characters">
-                {characters.map((characters,index) =>( // return implicito, es para no hacer un return dentro de un return.
+                {characters.map((character,index) =>( // return implicito, es para no hacer un return dentro de un return.
                     <div className="character-container" key={index}>
-                        <p>{characters.name}</p>
+                        
+                        <div>
+                            <img src={character.image} alt={character.name}></img>
+                        </div>
+                        <div>
+                            <h3>{character.name}</h3>
+                            <h6>
+                                {character.status === "Alive" ? (
+                                    <>
+                                    <span className="Alive" />
+                                    Alive
+                                    </>
+                                ):(
+                                    <>
+                                        <span className="dead"></span>
+                                        Dead
+                                    </>
+                                )}
+                            </h6>
+                            <p>
+                                <span className="text-grey">Episodios: </span>
+                                <span>{character.episode.length}</span>
+                            </p>
+                            <p>
+                                <span className="text-grey">Especie: </span>
+                                <span>{character.species}</span>
+                            </p>
+                        </div>
                     </div>
                 ))}
             </div>
-          
-
-
-            
-           
+                <span className="back-home">Home</span>
         </div>
        
     );
